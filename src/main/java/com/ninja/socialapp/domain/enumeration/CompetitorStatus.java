1657dfac30a3813e5c424d34e9b6d19a7920b3b9
mtdx <ms2asdk@gmail.com>
2017-06-05 13:45:51 -0700

    add competitor status

--- src/main/java/com/ninja/socialapp/domain/enumeration/CompetitorStatus.java
+++ src/main/java/com/ninja/socialapp/domain/enumeration/CompetitorStatus.java
@@ -4,5 +4,5 @@ package com.ninja.socialapp.domain.enumeration;
  * The CompetitorStatus enumeration.
  */
 public enum CompetitorStatus {
-    IN_PROGRESS,DONE
+    IDLE,IN_PROGRESS,DONE
 }
--- src/main/webapp/app/entities/competitor/competitor.model.ts
+++ src/main/webapp/app/entities/competitor/competitor.model.ts
@@ -1,5 +1,6 @@
 
 const enum CompetitorStatus {
+    'IDLE',
     'IN_PROGRESS',
     'DONE'
 
