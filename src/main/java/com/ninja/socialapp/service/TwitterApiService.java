942f593b1e6cb48e819c986cfa48804fef81d8b3
mtdx <ms2asdk@gmail.com>
2017-06-19 14:32:35 -0700

    remove extra import

--- src/main/java/com/ninja/socialapp/service/TwitterApiService.java
+++ src/main/java/com/ninja/socialapp/service/TwitterApiService.java
@@ -8,7 +8,6 @@ import com.ninja.socialapp.domain.enumeration.TwitterErrorType;
 import com.ninja.socialapp.domain.enumeration.TwitterStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
