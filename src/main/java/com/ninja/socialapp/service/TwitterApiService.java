9542f0f883ad5ad7743d9b3f922b1c40bd325196
mtdx <ms2asdk@gmail.com>
2017-08-03 00:00:14 -0700

    higher delays

--- src/main/java/com/ninja/socialapp/service/TwitterApiService.java
+++ src/main/java/com/ninja/socialapp/service/TwitterApiService.java
@@ -159,7 +159,7 @@ public class TwitterApiService {
                 if (twitterSettings.getNotLikeTweetsOlderThan() != 0 && difference <= twitterSettings.getNotLikeTweetsOlderThan()) {  // we only favorite tweets newer than x months
                     Long tweetId = tweet.getId();
                     String tweetText = tweet.getText();
-                    threadWait(getRandInt(3, 7));
+                    threadWait(getRandInt(6, 14));
 
                     if (twitterAccount.getRetweetAccount() == null && tweetText.length() >= 70
                         && getRandInt(1, 100) <= twitterSettings.getRetweetPercent()) {
