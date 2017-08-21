a54a58e51ea9fc220997f882c9f2826abb8e6bd6
mtdx <ms2asdk@gmail.com>
2017-08-21 06:40:12 -0700

    higher delays

--- src/main/java/com/ninja/socialapp/service/TwitterApiService.java
+++ src/main/java/com/ninja/socialapp/service/TwitterApiService.java
@@ -88,7 +88,7 @@ public class TwitterApiService {
         for (TwitterAccount twitterAccount : twitterAccounts) {
             final Twitter twitterClient = getTwitterInstance(twitterAccount);
             new Thread(() -> updateAccount(twitterAccount, twitterClient)).start();
-            threadWait(getRandInt(5, 7));
+            threadWait(getRandInt(6, 9));
         }
     }
 
@@ -149,7 +149,7 @@ public class TwitterApiService {
             log.error(ex.getMessage());
         }
         for (Long ID : followers) {
-            threadWait(getRandInt(5, 7));  // 180 per 15 min request limit
+            threadWait(getRandInt(6, 9));  // 180 per 15 min request limit
             if (isSpamAccount(ID, twitterClient, twitterAccount, twitterSettings))
                 continue;  // we try to target real accounts only
             try {
@@ -165,7 +165,7 @@ public class TwitterApiService {
                 if (twitterSettings.getNotLikeTweetsOlderThan() != 0 && difference <= twitterSettings.getNotLikeTweetsOlderThan()) {  // we only favorite tweets newer than x months
                     Long tweetId = tweet.getId();
                     String tweetText = tweet.getText();
-                    threadWait(getRandInt(60, 90));
+                    threadWait(getRandInt(90, 120));
 
                     if (twitterAccount.getRetweetAccount() == null && tweetText.length() >= 70
                         && getRandInt(1, 100) <= twitterSettings.getRetweetPercent()) {
@@ -239,7 +239,7 @@ public class TwitterApiService {
             try {
                 list = twitterClient.getFavorites(paging);
                 for (Status s : list) {
-                    threadWait(getRandInt(60, 90));
+                    threadWait(getRandInt(90, 120));
                     twitterClient.destroyFavorite(s.getId());
                 }
                 paging.setPage(paging.getPage() + 1);
